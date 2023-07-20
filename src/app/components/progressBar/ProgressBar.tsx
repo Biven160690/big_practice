ba339b9613d19a678f40a440f4b88adc232fc907
s_mamonko <s_mamonko@wargaming.net>
2023-07-20 10:59:00 +0300

    fixed remarks

--- src/app/components/progressBar/ProgressBar.tsx
+++ src/app/components/progressBar/ProgressBar.tsx
@@ -64,7 +64,7 @@ export const ProgressBar = () => {
     }, []);
 
     return (
-        <div className='prorr'>
+        <div className='progressBar'>
             <div
                 className={styles.container}
                 ref={containerRef}
