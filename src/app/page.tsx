ae7aecbd94c44f75b28434abd83d8a0104564839
s_mamonko <s_mamonko@wargaming.net>
2023-11-28 16:12:40 +0200

    added ScrollAnimation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import { ProgressBarSteps } from './components/50_projects/progressBarSteps';
 import { RotatingBG } from './components/50_projects/rotatingBG';
 import { HiddenSearch } from './components/50_projects/hiddenSearch';
 import { BlurryLoading } from './components/50_projects/blurryLoading';
+import { ScrollAnimation } from './components/50_projects/scrollAnimation';
 
 export default function App() {
     return (
@@ -54,6 +55,9 @@ export default function App() {
                 </div>
                 <div className={styles.blurry}>
                     <BlurryLoading />
+                </div>
+                <div className={styles.scrollAnimation}>
+                    <ScrollAnimation />
                 </div>
             </div>
         </div>
