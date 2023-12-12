0de85c07b86eb12b0f9881232869d79650650d1f
s_mamonko <s_mamonko@wargaming.net>
2023-12-12 09:25:31 +0200

    added drag n drop

---
+++ src/app/components/50_projects/dragNDrop/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import cx from 'classnames';
+
+export const DragNDrop = () => {
+    const [hoveredElement, setHoveredElement] = React.useState<number>(-1);
+    const [selectedCard, setSelectedCard] = React.useState<number>(0);
+
+    const onDragEnd = () => {
+        setSelectedCard(hoveredElement);
+        setHoveredElement(-1);
+    };
+
+    return (
+        <div className={styles.base}>
+            {new Array(5).fill(0).map((_, index) => (
+                <div
+                    key={index}
+                    className={cx(
+                        styles.card,
+                        hoveredElement === index && styles.card__dragOver
+                    )}
+                    onDragOver={() => setHoveredElement(index)}
+                >
+                    {index === selectedCard && (
+                        <div
+                            className={cx(
+                                styles.img,
+                                hoveredElement >= selectedCard && styles.img__startDrag,
+                                hoveredElement > index && styles.img__finishDrag,
+                            )}
+                            draggable
+                            onDragEnd={onDragEnd}
+                        />
+                    )}
+                </div>
+            ))}
+        </div>
+    );
+};
